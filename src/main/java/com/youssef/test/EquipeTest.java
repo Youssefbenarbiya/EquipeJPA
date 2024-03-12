c6ca3d3c25bbd94c6f14782e61966c31e733b98d
BENARBIA YOUSEF <benarbiayousef@nabeul.r-iset.tn>
2024-03-12 12:51:54 +0100

    Update EquipeTest.java

--- src/main/java/com/youssef/test/EquipeTest.java
+++ src/main/java/com/youssef/test/EquipeTest.java
@@ -1,21 +1,47 @@
 package com.youssef.test;
+
 import com.youssef.dao.EquipeDao;
 import com.youssef.entities.Equipe;
+
 public class EquipeTest {
-public static void main(String[] args) {
-//créer un objet client
-Equipe c = new Equipe();
-c.setNom("Youssef Benarbia");
-c.setVille("Nabeul");
-//ajouter l'objet client à la BD
-EquipeDao cltDao = new EquipeDao();
-cltDao.ajouter(c);
-System.out.println("Appel de la méthode listerTous");
-for (Equipe cl : cltDao.listerTous())
-System.out.println(cl.getCode()+" "+cl.getNom());
-System.out.println("Appel de la méthode listerParNom");
-for (Equipe cl : cltDao.listerParNom("Youss"))
-System.out.println(cl.getCode()+" "+cl.getNom());
-//tester les autres méthodes de la classe ClientDao
-}
+    public static void main(String[] args) {
+        // créer un objet equipe 1
+        Equipe equipe1 = new Equipe();
+        equipe1.setNom("fc Bercelone");
+        equipe1.setVille("Spain");
+
+        // créer un objet equipe 2
+        Equipe equipe2 = new Equipe();
+        equipe2.setNom("Paris saint germin");
+        equipe2.setVille("Paris");
+
+        // ajouter les équipes à la BD
+        EquipeDao equipeDao = new EquipeDao();
+        equipeDao.ajouter(equipe1);
+        equipeDao.ajouter(equipe2);
+
+        // Afficher les équipes avant modification
+        System.out.println("Appel de la méthode listerTous avant modification :");
+        for (Equipe equipe : equipeDao.listerTous())
+            System.out.println(equipe.getCode() + " " + equipe.getNom());
+
+        // Modifier l'équipe 1
+        equipe1.setNom("Real madrid");
+        equipeDao.modifier(equipe1);
+
+        // Afficher les équipes après modification de l'équipe 1
+        System.out.println("Appel de la méthode listerTous après modification :");
+        for (Equipe equipe : equipeDao.listerTous())
+            System.out.println(equipe.getCode() + " " + equipe.getNom());
+
+        // Supprimer l'équipe 2
+        equipeDao.supprimer(equipe2);
+
+        // Afficher les équipes après suppression de l'équipe 2
+        System.out.println("Appel de la méthode listerTous après suppression :");
+        for (Equipe equipe : equipeDao.listerTous())
+            System.out.println(equipe.getCode() + " " + equipe.getNom());
+
+       
+    }
 }
